b0fc5c3e3815dc47f52a2f8c562288313326e45f
Sugumar Deshikan <sukumar.deshikan@gmail.com>
2022-06-26 17:49:14 +0530

    crud operations

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Accessing data with MySQL using Spring Data JPA in 15 minutes
 
+GET: http://localhost:8080/users -- returns list of all users
+
+POST: http://localhost:8080/users -- create new user
+
+  {
+    "name": "Adaline Reichel",
+    "email": "Adaline.Reichel@gmail.com"
+  }
+
+PUT: http://localhost:8080/users/12 -- update user
+{
+    "id": 12,
+    "name": "Adaline Reichel",
+    "email": "Adaline.Reichel@gmail.com"
+  }
+
+DELETE: http://localhost:8080/users/12 -- delete user
+
 ## Outcomes:
 MySQL database, build a Spring application, and connect it to the newly created database.
 
+## Tools and technologies used:
 
+Java 17
+Spring Boot
+Spring Data JPA ( Hibernate)
+Lombok
+MySQL
+VS Code
+Maven
+Tomcat
+Thunder Client / Postman
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,33 +1,58 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(path = "/demo")
+@RequestMapping(path = "/users")
 public class MainController {
 
     @Autowired
     private UserRepository userRepository;
     
-    @PostMapping(path = "/add")
-    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+    @PostMapping
+    public @ResponseBody ResponseEntity<User> addUser(@RequestBody User userDetails) {
 
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        userRepository.save(n);
-        return "saved";
+        User u = new User();
+        u.setEmail(userDetails.getEmail());
+        u.setName(userDetails.getName());
+        userRepository.save(u);
+        return ResponseEntity.ok(u);
+        
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
+    @PutMapping(path = "{id}")
+    public @ResponseBody ResponseEntity<User> updateUser(@RequestBody User userDetails, @PathVariable(value = "id") Integer userId) {
+        User existingUser = userRepository.findById(userId).orElseThrow();
 
+        existingUser.setId(userDetails.getId());
+        existingUser.setEmail(userDetails.getEmail());
+        existingUser.setName(userDetails.getName());
+
+        final User updatedUser = userRepository.save(existingUser);
+        return ResponseEntity.ok(updatedUser);
+        
+    }
+
+    // get list of all users
+    @GetMapping
+    public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteById(@PathVariable(value = "id") Integer userId) {
+        userRepository.deleteById(userId);
+    }
+
+
 }
